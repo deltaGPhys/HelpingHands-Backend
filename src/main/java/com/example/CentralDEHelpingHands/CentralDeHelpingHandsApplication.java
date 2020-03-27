08d610da6312eb5ea93ea5c2db363fd9ad79781f
David Trombello <dtrombello@gmail.com>
2020-03-27 15:39:09 -0400

    initial commit

---
+++ src/main/java/com/example/CentralDEHelpingHands/CentralDeHelpingHandsApplication.java
@@ -0,0 +1,13 @@
+package com.example.CentralDEHelpingHands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDeHelpingHandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDeHelpingHandsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CentralDEHelpingHands/CentralDeHelpingHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CentralDEHelpingHands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeHelpingHandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
