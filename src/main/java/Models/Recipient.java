5f17102af34094e020e6522636a25d737a2f31b4
David Trombello <dtrombello@gmail.com>
2020-03-27 15:48:40 -0400

    update recipient model:

--- src/main/java/Models/Recipient.java
+++ src/main/java/Models/Recipient.java
@@ -1,4 +1,35 @@
 package Models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+
+@Entity
 public class Recipient {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNum;
+    @Email
+    private String email;
+    private String areaOfNeed;
+    private String password;
+    private String location;
+
+    public Recipient() {
+    }
+
+    public Recipient(Long id, String firstName, String lastName, String phoneNum, String email, String areaOfNeed, String password, String location) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.areaOfNeed = areaOfNeed;
+        this.password = password;
+        this.location = location;
+    }
 }
