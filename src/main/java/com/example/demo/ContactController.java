f1930036168afba1da6ee233ff2b346d0405edaa
David Trombello <dtrombello@gmail.com>
2020-04-09 19:49:04 -0400

    added annotation to controller, creating a new contact now

--- src/main/java/com/example/demo/ContactController.java
+++ src/main/java/com/example/demo/ContactController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,7 @@ public class ContactController {
     private ContactService contactService;
 
     @PostMapping("/send-email")
-    public ResponseEntity<Contact> saveAndEmailContact (Contact contact){
+    public ResponseEntity<Contact> saveAndEmailContact (@RequestBody Contact contact){
         return new ResponseEntity<>(contactService.createContact(contact), HttpStatus.OK);
     }
 
