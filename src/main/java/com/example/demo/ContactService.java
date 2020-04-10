599ed49ab394a28976188b01a538d4a0883e88ee
David Trombello <dtrombello@gmail.com>
2020-04-09 20:19:53 -0400

    updating contact service

--- src/main/java/com/example/demo/ContactService.java
+++ src/main/java/com/example/demo/ContactService.java
@@ -14,28 +14,14 @@ public class ContactService {
     }
 
     public Contact createContact (Contact contact) {
-        Contact newContact = new Contact();
-        newContact.setFirstName(contact.getFirstName());
-        newContact.setLastName(contact.getLastName());
-        newContact.setEmail(contact.getEmail());
-        newContact.setPhoneNum(contact.getPhoneNum());
-        newContact.setBirthDate(contact.getBirthDate());
-        newContact.setReasonForContact(contact.getReasonForContact());
-        newContact.setPreferredApptTime(contact.getPreferredApptTime());
-        newContact.setMessage(contact.getMessage());
-        return contactRepository.save(newContact);
 
-
-
-//        Boolean emailSent = null;
-//        System.out.println("New Contact: " + contact);
-//        //System.out.println("To Email" + contactToEmail);
-//         emailSent = SendMail.sendMessage(contact.getFirstName(),
-//                 contact.getLastName(), contact.getEmail(), contact.getPhoneNum(),
-//                 contact.getBirthDate(), contact.getReasonForContact(), contact.getPreferredApptTime(),
-//                 contact.getMessage());
-        //return contactRepository.save(contact);
-        //return emailSent;
+        Boolean emailSent = null;
+         SendMail.sendMessage(contact.getFirstName(),
+                 contact.getLastName(), contact.getEmail(), contact.getPhoneNum(),
+                 contact.getBirthDate(), contact.getReasonForContact(), contact.getPreferredApptTime(),
+                 contact.getMessage());
+        return contactRepository.save(contact);
+//        return emailSent;
     }
 
 
